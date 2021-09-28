634b0e58db96207a11c31f7024ee7ec3e5864fc1
Marian Tang <54853602+mtanggg@users.noreply.github.com>
2021-09-27 21:51:58 -0400

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -35,23 +35,6 @@ $(document).ready(function() {
     });
   });
 
-  /* originally Jonas got from "https://css-tricks.com/snippets/jquery/smooth-scrolling/"
-  however, it has changed so I typed out his code from lecture vid
-    $(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.has.slice(1) + ']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-    });
-  });*/
-
   /* ANIMATIONS ON SCROLL */
   $(".js--wp-1").waypoint(function(direction) {
       $(".js--wp-1").addClass("animate__animated animate__fadeIn");
